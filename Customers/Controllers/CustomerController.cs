bd02618dc5bc564e0bfbe0081c0b1326451d37da
giolga <giolga.kumi@gmail.com>
2024-08-10 16:39:13 +0400

    Started writing api

---
+++ Customers/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using Customers.Model;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Customers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public CustomerController(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            var connection = new SqlConnection(_config.GetConnectionString("CustomerConnection"));
+
+            var customers = await connection.QueryAsync<Customer>("SELECT * FROM Customer");
+
+            return Ok(customers);
+        }
+    }
+}
---
+++ Customers/Model/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Customers.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
